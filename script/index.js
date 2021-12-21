edd66b566b7ed9ed999793a11947d2408e194ecf
AndreySitnikov34 <ac1970@yandex.ru>
2021-12-21 22:24:15 +0300

    2112 ToFirstRewiew

--- script/index.js
+++ script/index.js
@@ -1,7 +1,5 @@
-// console.log('Поехали! © Ю.А.Гагарин')
 const popupFormUser = document.querySelector('.popup-form-user');
 const cardFormPopup = document.querySelector('.popup-form-card');
-const cardForm = document.querySelector('.popup-form-card');
 const popupImage = document.querySelector('.popup-image');
 const imageOpen = document.querySelector('.popup__image');
 const signImage = document.querySelector('.popup__image-alt');
@@ -18,7 +16,6 @@ const secondCellCard= document.querySelector('#second-cell-card');
 const cards = document.querySelector('.content');
 
 function addUserHandler(evt) {
-    // console.log("Обработчик юзера");
     evt.preventDefault();
     userName.textContent = formUserFirstCell.value;
     userAbout.textContent = formUserSecondCell.value;
@@ -26,10 +23,33 @@ function addUserHandler(evt) {
 }
 
 function addUser() {
-    // console.log("Редактировать юзера");
     popupFormUser.addEventListener('submit', addUserHandler);
-    popupFormUser.classList.remove('popup_fade_out');
-    popupFormUser.classList.add('popup_fade_in');
+    openPopup(popupFormUser);
+}
+
+function addNewCardHandler (evt) {
+    evt.preventDefault();
+    addCard({
+        cardName: firstCellCard.value,
+        cardLink: secondCellCard.value
+    })
+    firstCellCard.value = firstCellCard.defaultValue;
+    secondCellCard.value = secondCellCard.defaultValue;
+    closePopup();
+}
+
+function addNewCard () {
+    cardFormPopup.addEventListener('submit', addNewCardHandler);
+    openPopup(cardFormPopup);
+}
+
+function imagePopup (evt) {
+    popupImage.addEventListener('click',closePopup);
+    imageOpen.src='';
+    imageOpen.src=evt.target.src;
+    imageOpen.alt=evt.target.alt;
+    signImage.textContent = evt.target.alt;
+    openPopup(popupImage);
 }
 
 const initialCards = [
@@ -63,7 +83,7 @@ const initialCards = [
 const addCard = (card) => {
     const contentCard = `
   <article class="card">
-    <img src=${card.cardLink} alt=${card.cardName} class="card__img">
+    <img src=${card.cardLink} alt='${card.cardName}' class="card__img">
     <button type="button" class="card__del"></button>
     <div class="card__subscribe">
       <h2 class="card__location">${card.cardName}</h2>
@@ -85,50 +105,24 @@ function setContent () {
 
 setContent ()
 
-function addNewCardHandler (evt) {
-    // console.log("Обработчик карточки");
-    evt.preventDefault();
-    addCard({
-        cardName: firstCellCard.value,
-        cardLink: secondCellCard.value
-    })
-    console.log(secondCellCard);
-    firstCellCard.value = firstCellCard.defaultValue;
-    secondCellCard.value = secondCellCard.defaultValue;
-    closePopup(evt);
-}
-
-function addNewCard () {
-    // console.log("Добавить карточку");
-    cardForm.addEventListener('submit', addNewCardHandler);
-    cardFormPopup.classList.remove('popup_fade_out');
-    cardFormPopup.classList.add('popup_fade_in');
-}
-
 function removeCard (evt) {
-    console.log('GARBAGE!');
     evt.target.parentNode.remove();
 }
 
 function addToFavorite(evt) {
-    // console.log('Лайк!', evt.target, evt);
     evt.target.classList.toggle('card__heart_liked');
 }
 
-function openPopup() {
-    // console.log('Поп-ап, откройся!');
+function openPopup(popupElement) {
+    popupElement.classList.remove('popup_fade_out');
+    popupElement.classList.add('popup_fade_in');
 }
 
 function closePopup() {
-    // console.log('Поп-ап, закройся!');
     popupFormUser.classList.remove('popup_fade_in');
     popupFormUser.classList.add('popup_fade_out');
     cardFormPopup.classList.remove('popup_fade_in');
     cardFormPopup.classList.add('popup_fade_out');
-}
-
-function closeImagePopup() {
-    // console.log('Картинка, закройся!');
     popupImage.classList.remove('popup_fade_in');
     popupImage.classList.add('popup_fade_out');
 }
@@ -139,27 +133,12 @@ function keyUpHandler() {
     }
 }
 
-function popupImageHandler() {
-    // console.log('Обработчик картинки');
-    closeImagePopup();
-}
-
-function imagePopup (evt) {
-    // console.log("Открыть картинку в полный рост");
-    popupImage.addEventListener('click',popupImageHandler);
-    imageOpen.src='';
-    imageOpen.src=evt.target.src;
-    imageOpen.alt=evt.target.alt;
-    signImage.textContent = evt.target.alt;
-    popupImage.classList.remove('popup_fade_out');
-    popupImage.classList.add('popup_fade_in');
-    openPopup();
-}
-
 window.addEventListener("keyup", keyUpHandler);
 cardAddButton.addEventListener("click", addNewCard);
 formCloseButton.addEventListener("click", closePopup);
 formUserElement.addEventListener('submit', openPopup);
 userEditButton.addEventListener('click', addUser);
-document.querySelectorAll('.popup__button-close').forEach((element) => {element.addEventListener('click', closePopup)});
+document
+    .querySelectorAll('.popup__button-close')
+    .forEach((element) => element.addEventListener('click', closePopup));
 
